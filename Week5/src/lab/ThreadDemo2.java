27199e8cca35d432bd984fcf7e895d7900141453
Vinoth N <83577465+Vinoth170@users.noreply.github.com>
2021-07-11 17:23:47 +0530

    Update

--- Week5/src/lab/ThreadDemo2.java
+++ Week5/src/lab/ThreadDemo2.java
@@ -4,12 +4,12 @@ import java.util.Date;
 
 public class ThreadDemo2 {
 	public static void main(String[] args) throws Exception{
-		GrossaryShop grossary=new GrossaryShop();
+		GroceryShop grocery=new GroceryShop();
 		Thread t=Thread.currentThread();
 		t.setName("Karthick");
-		Thread ramu=new Thread(new ToServant(grossary), "Ramu");
+		Thread ramu=new Thread(new ToServant(grocery), "Ramu");
 		System.out.println("Time: "+new Date());
-//		grossary.buyGrossary();
+//		grocery.buyGrocery();
 		ramu.start();
 		System.out.println(t.getName()+" entered meeting at "+new Date());
 		
@@ -18,29 +18,29 @@ public class ThreadDemo2 {
 
 class ToServant implements Runnable{
 	
-	GrossaryShop grossary;
-	public ToServant(GrossaryShop grossary) {
-		this.grossary=grossary;
+	GroceryShop grocery;
+	public ToServant(GroceryShop grocery) {
+		this.grocery=grocery;
 	}
 	
 	@Override
 	public void run(){
 		try {
-			grossary.buyGrossary();
+			grocery.buyGrocery();
 		}catch(Exception ex) {
 			ex.printStackTrace();
 		}
 	}
 }
 
-class GrossaryShop{
-	void buyGrossary() throws Exception{
+class GroceryShop{
+	void buyGrocery() throws Exception{
 		Thread t=Thread.currentThread();
 		for(int i=0; i<5;i++) {
 			System.out.println(i);
 			Thread.sleep(1000);
 		}
-		System.out.println(t.getName()+ " wait at grossary shop for 5 secs...");
-		System.out.println(t.getName()+" went to grossary shop");
+		System.out.println(t.getName()+ " wait at grocery shop for 5 secs...");
+		System.out.println(t.getName()+" went to grocery shop");
 	}
 }
